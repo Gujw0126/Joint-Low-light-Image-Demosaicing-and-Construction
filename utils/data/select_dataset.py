e8344e22b1b650e3a1f995a79b0f827a013dca2a
Gujw0126 <jingwengu@hust.edu.cn>
2025-02-25 19:03:26 +0800

    add others

---
+++ utils/data/select_dataset.py
@@ -0,0 +1,28 @@
+
+'''
+# --------------------------------------------
+# select dataset
+# --------------------------------------------
+# Kai Zhang (github: https://github.com/cszn)
+# --------------------------------------------
+'''
+def define_Dataset(dataset_opt):
+    dataset_type = dataset_opt['dataset_type'].lower()
+
+    # -----------------------------------------
+    # Patterned flash reconstruction
+    # -----------------------------------------
+    if dataset_type in ['pf_data']:
+        from data.pf_data import DatasetPF as D
+
+    # -----------------------------------------
+    # Patterned flash/no-flash reconstruction
+    # -----------------------------------------
+    elif dataset_type in ['fnf_data']:
+        from data.fnf_data import DatasetFnF as D
+    else:
+        raise NotImplementedError('Dataset [{:s}] is not found.'.format(dataset_type))
+
+    dataset = D(dataset_opt)
+    print('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__, dataset_opt['name']))
+    return dataset
